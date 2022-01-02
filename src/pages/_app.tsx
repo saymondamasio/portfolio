923fbaedd803be665ebff0c09ec194c014f1bd46
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-02 17:47:48 -0300

    feat: config chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-
 import '../styles/global.scss'
+import { theme } from '../styles/theme'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
---
+++ src/styles/theme.ts
@@ -0,0 +1,32 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    green:{
+      300: '#00DF5E'
+    },
+    white: {
+      200: '#F9F9F9'
+    },
+    gray: {
+      100: '#828282',
+      700: '#333333',
+      900: '#212121'
+    },
+    black: {
+      900: '#171717'
+    }
+  },
+  fonts: {
+    body: '"Roboto", sans-serif',
+    heading: '"Roboto", sans-serif',
+  },
+  styles: {
+    global: {
+      body: {
+        color: 'gray.100',
+        backgroundColor: 'black.900',
+      }
+    }
+  }
+})
