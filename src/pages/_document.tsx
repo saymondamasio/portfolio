d986b2be8f0c925f8f55a63820a3e6dce1713053
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-02 17:48:43 -0300

    feat: add font DM Sans

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,11 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
 
             <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-            <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300;400;700&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
