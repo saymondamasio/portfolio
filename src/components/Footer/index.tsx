3722c099c7b9c28187a8da465671ef200276f52a
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-02 20:57:53 -0300

    refactor: change directory

--- src/components/Footer/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.scss'
-
-export function Footer() {
-  return (
-    <footer className={styles.container}>
-      <p>Feito com 💜 por Saymon Damásio</p>
-    </footer>
-  )
-}
