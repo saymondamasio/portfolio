1b55ee7a74ca9f2bb78bcfb8f83d597144826502
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-03 20:31:54 -0300

    feat: config swiper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import 'swiper/css'
+import 'swiper/css/bundle'
+import '../styles/slider-swiper.scss'
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
