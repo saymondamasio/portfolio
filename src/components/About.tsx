2db24c98a0ac72446f00f96db53aff256052f2f7
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-03 16:45:17 -0300

    feat: add About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,10 @@
-import { Flex, FlexProps, Heading, Text } from '@chakra-ui/react'
+import { Flex, FlexProps, Heading, Image, Link, Text } from '@chakra-ui/react'
 
 type Props = FlexProps
 
 export function About({ ...rest }: Props) {
   return (
-    <Flex justify="center" {...rest}>
+    <Flex align="center" direction="column" {...rest}>
       <Flex maxW="745px" w="100%" direction="column" align="center">
         <Heading fontSize="3xl">Sobre mim</Heading>
 
@@ -16,6 +16,92 @@ export function About({ ...rest }: Props) {
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident
         </Text>
+      </Flex>
+
+      <Flex mt="20" maxW="800px" w="100%">
+        <Flex w="100%" justify="space-between">
+          <Flex direction="column" align="center">
+            <Flex
+              w="62px"
+              h="62px"
+              borderRadius="full"
+              bgColor="gray.900"
+              align="center"
+              justify="center"
+            >
+              <Image src="/images/smile.svg" alt="Carrinha sorridente" />
+            </Flex>
+            <Text fontWeight="bold" fontSize="lg" mt="19px">
+              Meu nome
+            </Text>
+            <Text color="gray.100" mt="5px">
+              Saymon Damásio
+            </Text>
+          </Flex>
+          <Flex direction="column" align="center">
+            <Flex
+              w="62px"
+              h="62px"
+              borderRadius="full"
+              bgColor="gray.900"
+              align="center"
+              justify="center"
+            >
+              <Image src="/images/mail.svg" alt="Carrinha sorridente" />
+            </Flex>
+            <Text fontWeight="bold" fontSize="lg" mt="19px">
+              E-mail
+            </Text>
+            <Link
+              href="mailto:saymon.damasio95@gmail.com"
+              color="gray.100"
+              mt="5px"
+            >
+              saymon.damasio95@gmail.com
+            </Link>
+          </Flex>
+          <Flex direction="column" align="center">
+            <Flex
+              w="62px"
+              h="62px"
+              borderRadius="full"
+              bgColor="gray.900"
+              align="center"
+              justify="center"
+            >
+              <Image src="/images/instagram.svg" alt="Carrinha sorridente" />
+            </Flex>
+            <Text fontWeight="bold" fontSize="lg" mt="19px">
+              Instagram
+            </Text>
+            <Link
+              href="https://www.instagram.com/saymondamasio/"
+              target="_blank"
+              color="gray.100"
+              mt="5px"
+            >
+              @saymondamasio
+            </Link>
+          </Flex>
+          <Flex direction="column" align="center">
+            <Flex
+              w="62px"
+              h="62px"
+              borderRadius="full"
+              bgColor="gray.900"
+              align="center"
+              justify="center"
+            >
+              <Image src="/images/phone.svg" alt="Carrinha sorridente" />
+            </Flex>
+            <Text fontWeight="bold" fontSize="lg" mt="19px">
+              Telefone
+            </Text>
+            <Link href="tel:+5535991528780" color="gray.100" mt="5px">
+              (35) 99152-8780
+            </Link>
+          </Flex>
+        </Flex>
       </Flex>
     </Flex>
   )
