4109706c101a6df288736a437fad76ccb9e80b74
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-04 21:56:33 -0300

    feat: create contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,9 @@
+import { Flex } from '@chakra-ui/react'
+
+export default function Contact() {
+  return (
+    <Flex as="main" flex="1" justify="center">
+      <Flex maxW="1140px" w="100%" direction="column" px="10"></Flex>
+    </Flex>
+  )
+}
