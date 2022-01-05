e96e7fcbb0615b0c8cb7052ea9f6a55efc095f0e
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-04 22:01:36 -0300

    feat: config react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,25 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Flex } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import 'swiper/css'
 import 'swiper/css/bundle'
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
 import '../styles/slider-swiper.scss'
 import { theme } from '../styles/theme'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Flex minH="100vh" as="main" direction="column">
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </Flex>
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
