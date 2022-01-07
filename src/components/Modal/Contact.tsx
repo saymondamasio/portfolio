ce1ae42a9ce26f5607c95dd065a5c9b177758872
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-07 09:35:20 -0300

    feat: remove page contact and add modal contact

---
+++ src/components/Modal/Contact.tsx
@@ -0,0 +1,101 @@
+import {
+  Button,
+  Icon,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+} from '@chakra-ui/react'
+import {
+  RiGithubFill,
+  RiInstagramFill,
+  RiLinkedinBoxFill,
+  RiRocket2Fill,
+} from 'react-icons/ri'
+
+interface Props {
+  isOpen: boolean
+  onClose: () => void
+}
+
+export function Contact({ isOpen, onClose }: Props) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent
+        alignSelf="center"
+        bgColor="gray.900"
+        w="90%"
+        maxW="300px"
+        p="30px"
+      >
+        <ModalHeader p="0">Conecte-se</ModalHeader>
+        <ModalCloseButton top="30px" right="30px" />
+        <ModalBody mt="10" p="0" w="100%">
+          <Stack>
+            <Button
+              as="a"
+              href="https://www.linkedin.com/in/saymondamasio"
+              target="_blank"
+              w="100%"
+              bgColor="green.400"
+              variant="ghost"
+              justifyContent="flex-start"
+              leftIcon={
+                <Icon
+                  as={RiLinkedinBoxFill}
+                  boxSize="20px"
+                  marginRight="20px"
+                />
+              }
+            >
+              LinkedIn
+            </Button>
+            <Button
+              as="a"
+              href="https://github.com/saymondamasio"
+              target="_blank"
+              justifyContent="flex-start"
+              w="100%"
+              bgColor="green.400"
+              leftIcon={
+                <Icon as={RiGithubFill} boxSize="20px" marginRight="20px" />
+              }
+            >
+              Github
+            </Button>
+            <Button
+              as="a"
+              href="https://www.instagram.com/saymondamasio95"
+              target="_blank"
+              w="100%"
+              bgColor="green.400"
+              justifyContent="flex-start"
+              leftIcon={
+                <Icon as={RiInstagramFill} boxSize="20px" marginRight="20px" />
+              }
+            >
+              Instagram
+            </Button>
+            <Button
+              as="a"
+              href="https://app.rocketseat.com.br/me/saymondamasio"
+              target="_blank"
+              justifyContent="flex-start"
+              w="100%"
+              bgColor="green.400"
+              leftIcon={
+                <Icon as={RiRocket2Fill} boxSize="20px" marginRight="20px" />
+              }
+            >
+              Rocketseat
+            </Button>
+          </Stack>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+}
