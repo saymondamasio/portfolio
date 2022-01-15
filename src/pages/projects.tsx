893fa6dda041b429e506ea18685ac69401eedd21
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-15 17:09:07 -0300

    feat: add project surveys-api

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,17 +1,10 @@
-import {
-  Box,
-  Flex,
-  Grid,
-  GridItem,
-  Heading,
-  useDisclosure,
-} from '@chakra-ui/react'
+import { Flex, Grid, GridItem, Heading, useDisclosure } from '@chakra-ui/react'
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { useState } from 'react'
 import { v4 } from 'uuid'
 import { CardProject } from '../components/CardProject'
-import { ViewProjectModal } from '../components/Modal/ViewProjectModal'
+import { ViewProject } from '../components/Modal/ViewProject'
 import { api } from '../services/api'
 
 const repo_names = {
@@ -27,7 +20,14 @@ const repo_names = {
     'redux',
     'rocket-socket',
   ],
-  back: ['rentx-api', 'gobarber-api', 'valoriza-api', 'certificate', 'todos'],
+  back: [
+    'rentx-api',
+    'valoriza-api',
+    'gobarber-api',
+    'surveys-api',
+    'certificate',
+    'todos',
+  ]
 }
 
 type Tech = {
@@ -48,10 +48,7 @@ interface Project {
 }
 
 interface Props {
-  projects: {
-    front: Project[]
-    back: Project[]
-  }
+  projects: Project[]
 }
 
 export default function Projects({ projects }: Props) {
@@ -73,53 +70,24 @@ export default function Projects({ projects }: Props) {
         <Flex maxW="1140px" w="100%" px="10" direction="column" align="center">
           <Heading fontSize="3xl">Projetos</Heading>
 
-          <Box mt="10">
-            <Heading as="h2" fontSize="2xl">
-              Front-end
-            </Heading>
-            <Grid
-              mt="30px"
-              templateColumns={{
-                sm: 'repeat(auto-fit, minmax(300px,1fr))',
-                lg: 'repeat(3, 1fr)',
-              }}
-              gap="5"
-            >
-              {projects.front?.map(project => (
-                <GridItem key={project.id}>
-                  <CardProject
-                    project={project}
-                    handleOpenModal={() => handleOpenModal(project)}
-                  />
-                </GridItem>
-              ))}
-            </Grid>
-          </Box>
-
-          <Box mt="10">
-            <Heading as="h2" fontSize="2xl">
-              Back-end
-            </Heading>
-            <Grid
-              mt="30px"
-              templateColumns={{
-                sm: 'repeat(auto-fit, minmax(300px,1fr))',
-                lg: 'repeat(3, 1fr)',
-              }}
-              gap="5"
-            >
-              {projects.back?.map(project => (
-                <GridItem key={project.id}>
-                  <CardProject
-                    project={project}
-                    handleOpenModal={() => handleOpenModal(project)}
-                  />
-                </GridItem>
-              ))}
-            </Grid>
-          </Box>
-
-          <ViewProjectModal
+          <Grid
+            mt="30px"
+            templateColumns={{
+              sm: 'repeat(auto-fit, minmax(300px,1fr))',
+              lg: 'repeat(3, 1fr)',
+            }}
+            gap={{ base: '5', lg: '10' }}
+          >
+            {projects?.map(project => (
+              <GridItem key={project.id}>
+                <CardProject
+                  project={project}
+                  handleOpenModal={() => handleOpenModal(project)}
+                />
+              </GridItem>
+            ))}
+          </Grid>
+          <ViewProject
             isOpen={isOpen}
             onClose={onClose}
             project={projectSelected!}
@@ -131,33 +99,9 @@ export default function Projects({ projects }: Props) {
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  const projects_back: Project[] = []
-  const projects_front: Project[] = []
+  const projects: Project[] = []
 
-  for await (const repo_name of repo_names.back) {
-    let project_info
-
-    try {
-      const response = await api.get(
-        `https://raw.githubusercontent.com/saymondamasio/${repo_name}/main/project-info.json`
-      )
-
-      project_info = response.data
-      projects_back.push({
-        id: v4(),
-        name: project_info.name,
-        short_description: project_info.short_description,
-        description: project_info.description,
-        images: project_info.images || [],
-        videos: project_info.videos || [],
-        techs: project_info.techs || [],
-        link_preview: project_info.link_preview || null,
-        link_repo: `https://github.com/saymondamasio/${repo_name}`,
-      })
-    } catch {}
-  }
-
-  for await (const repo_name of repo_names.front) {
+  for await (const repo_name of repo_names) {
     let project_info
 
     try {
@@ -165,7 +109,7 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
         `https://raw.githubusercontent.com/saymondamasio/${repo_name}/main/project-info.json`
       )
       project_info = response.data
-      projects_front.push({
+      projects.push({
         id: v4(),
         name: project_info.name,
         short_description: project_info.short_description,
@@ -181,10 +125,7 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 
   return {
     props: {
-      projects: {
-        front: projects_front,
-        back: projects_back,
-      },
+      projects,
     },
     revalidate: false,
   }
